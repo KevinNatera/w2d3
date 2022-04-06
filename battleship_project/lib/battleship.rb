c0bd02cc34a6a031e6ec61c9b4f4e7c0ec7e5bd9
Kevin <NateraKevin1@gmail.com>
2022-04-06 12:59:36 -0400

    completed battleship project

--- battleship_project/lib/battleship.rb
+++ battleship_project/lib/battleship.rb
@@ -2,5 +2,57 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+    attr_reader :player , :board
+    
+    def initialize(n)
+        @player = Player.new
+        @board = Board.new(n)
+        @remaining_misses = @board.size / 2
+    end
 
+
+    def start_game
+        @board.place_random_ships
+        puts @board.num_ships
+        @board.print
+    end
+
+
+    def lose?
+        if @remaining_misses <= 0
+            puts "you lose"
+            true
+        else
+            false
+        end
+    end
+    
+
+    def win?
+        if @board.num_ships == 0
+            puts "you win"
+            true
+        else
+            false
+        end
+    end
+
+    
+    def game_over?
+        if self.win? || self.lose?
+            true
+        else
+            false
+        end
+    end
+
+
+    def turn
+        position = @player.get_move
+        @board.print
+        if !@board.attack(position)
+            @remaining_misses -= 1
+        end
+        puts @remaining_misses
+    end
 end
